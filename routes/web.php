009bd2aafd2ba77fe29595b0e075cb998737b753
DAEYEOP <daeval@hotmail.com>
2020-01-07 12:45:28 +0900

    [laravel]사용자 인증

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,12 @@
 |
 */
 
+/*
 Route::get('/', function () {
     $data = ['A','B','C'];
     return view('test', ['name' => 'daeyeop','data' => $data]);
 });
-
+*/
 /*
 Route::get('/',['as'=>'home',function(){
     return '제 이름은 home 입니다.';
@@ -39,4 +40,36 @@ Route::get('/{param}',function($param="test"){
  * Controller
  */
 Route::get('/', 'WelcomeController@index');
-Route::resource('articles', 'ArticlesController');
+//Route::resource('articles', 'ArticlesController');
+
+Route::get('auth/login', function (){
+    $credentials = [
+        'email' => 'kgty4@forbiz.co.kr',
+        'password' => 'password'
+        ];
+
+    if(! auth()->attempt($credentials)){
+        return "로그인 정보가 정확하지 않습니다";
+    }
+
+    return redirect('protected');
+});
+
+Route::get('protected', function (){
+    dump(session()->all());
+
+    if(! auth()->check()){
+        return "누구세요?";
+    }
+
+    return "어서 오세요".auth()->user()->name;
+});
+
+Route::get('auth/logout', function (){
+    auth()->logout();
+
+    return "또 봐요~";
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
